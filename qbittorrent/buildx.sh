3f48bbedbfdb8bfc69140303b9ce7db94813534a
anonymous <anonymous@hotmail.com>
2021-06-17 13:27:03 +0800

    update

--- qbittorrent/buildx.sh
+++ qbittorrent/buildx.sh
@@ -10,14 +10,15 @@ RELEASE_SEMVER=${QB_FULL_VERSION}
 PATCH_SEMVER=$(printf "${RELEASE_SEMVER}" | cut -d '.' -f 1-3)
 MINOR_SEMVER=$(printf "${RELEASE_SEMVER}" | cut -d '.' -f 1-2)
 MAJOR_SEMVER=$(printf "${RELEASE_SEMVER}" | cut -d '.' -f 1)
+
+## 多平台标签数组
 if [[ $RELEASE_SEMVER == $PATCH_SEMVER ]]; then
     ALL_MULTIARCH_TAG=( ${MAJOR_SEMVER} ${MINOR_SEMVER} ${RELEASE_SEMVER} latest )
 else
     ALL_MULTIARCH_TAG=( ${MAJOR_SEMVER} ${MINOR_SEMVER} ${PATCH_SEMVER} ${RELEASE_SEMVER} latest )
 fi
 
-## 构建
-declare -a IMAGES
+## 构建镜像
 IMAGES=()
 for arch in "${BUILDX_ARCH[@]}"; do
     cmd_tag=""
@@ -45,7 +46,7 @@ for arch in "${BUILDX_ARCH[@]}"; do
     done
 done
 
-## 增加manifest
+## 添加多平台标签
 for tag in ${ALL_MULTIARCH_TAG[@]}; do
     docker manifest create "${DOCKERHUB_REPOSITORY}:${tag}" "${IMAGES[@]}"
     docker manifest annotate "${DOCKERHUB_REPOSITORY}:${tag}" "${DOCKERHUB_REPOSITORY}:${RELEASE_SEMVER}-arm-v6" --variant "v6"
@@ -53,7 +54,7 @@ for tag in ${ALL_MULTIARCH_TAG[@]}; do
     docker manifest annotate "${DOCKERHUB_REPOSITORY}:${tag}" "${DOCKERHUB_REPOSITORY}:${RELEASE_SEMVER}-arm64" --variant "v8"
 done
 
-## 推送manifest
+## 推送多平台标签
 for tag in ${ALL_MULTIARCH_TAG[@]}; do
     docker manifest push --purge "${DOCKERHUB_REPOSITORY}:${tag}"
 done
