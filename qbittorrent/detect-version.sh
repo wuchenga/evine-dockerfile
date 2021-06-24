98c0e20122ad89c7be57067fa8b17681c5a1717b
anonymous <anonymous@hotmail.com>
2021-06-24 16:08:19 +0800

    增加自动检测官方版本的脚本，当有更新时自动开始构建并推送

--- qbittorrent/detect-version.sh
+++ qbittorrent/detect-version.sh
@@ -21,10 +21,12 @@ if [[ $ver_qb_official != $ver_qb_local ]]; then
     . $dir_myscripts/notify.sh
     . $dir_myscripts/my_config.sh
     notify "qBittorrent已经升级" "当前官方版本信息如下：\nqBittorrent: ${ver_qb_official}\nlibtorrent: ${ver_lib_official}\n\n当前本地版本信息如下：\nqBittorrent: ${ver_qb_local}\nlibtorrent: ${ver_lib_local}\n\n已自动开始重新构建并推送镜像..."
+    start_time=$(date +'%Y-%m-%d %H:%M:%S')
     ./buildx-run.sh "$ver_qb_official" "$ver_lib_official" && {
         echo "$ver_qb_official" > ./ver_qbittorrent
         echo "$ver_lib_official" > ./ver_libtorrent
-    }
+        notify "qBittorrent镜像构建成功" "当前版本信息如下：\nqBittorrent: ${ver_qb_official}\nlibtorrent: ${ver_lib_official}\n\n构建时间如下：\n开始时间：${start_time}\n完成时间：$(date +'%Y-%m-%d %H:%M:%S')\n\nhub链接如下：\nhttps://hub.docker.com/repository/docker/nevinee/qbittorrent\nhttps://hub.docker.com/r/nevinee/qbittorrent"
+    } || notify "qBittorrent镜像构建失败" "当前官方版本信息如下：\nqBittorrent: ${ver_qb_official}\nlibtorrent: ${ver_lib_official}\n\n当前本地版本信息如下：\nqBittorrent: ${ver_qb_local}\nlibtorrent: ${ver_lib_local}\n\n构建时间如下：\n开始时间：${start_time}\n完成时间：$(date +'%Y-%m-%d %H:%M:%S')"
 else
     echo "qBittorrent官方版本和本地一致，均为：$ver_qb_official"
 fi
