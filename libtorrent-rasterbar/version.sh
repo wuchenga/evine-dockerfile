20e3eddacd502c09d1c0c29c159ae7cf810e55c5
anonymous <anonymous@hotmail.com>
2021-06-28 20:02:56 +0800

    update

--- libtorrent-rasterbar/version.sh
+++ libtorrent-rasterbar/version.sh
@@ -8,9 +8,9 @@ dir_myscripts=$(cd $(dirname $0); cd ../../myscripts; pwd)
 cd $dir_shell
 
 ## 官方版本
-ver_lib_official=$(curl -s https://api.github.com/repos/arvidn/libtorrent/tags | jq -r .[]."name" | grep -vi "rc" | grep -m 1 "v1." | sed "s/v//")
-ver_lib1_official=$(echo $ver_lib_official | jq -r .[]."name" | grep -vi "rc" | grep -m 1 "v1." | sed "s/v//")
-ver_lib2_official=$(echo $ver_lib_official | jq -r .[]."name" | grep -vi "rc" | grep -m 1 "v2." | sed "s/v//")
+lib_tag=$(curl -s https://api.github.com/repos/arvidn/libtorrent/tags)
+ver_lib1_official=$(echo $lib_tag | jq -r .[]."name" | grep -vi "rc" | grep -m 1 "v1." | sed "s/v//")
+ver_lib2_official=$(echo $lib_tag | jq -r .[]."name" | grep -vi "rc" | grep -m 1 "v2." | sed "s/v//")
 
 ## 本地版本
 ver_lib1_local=$(cat ./1.x.version)
